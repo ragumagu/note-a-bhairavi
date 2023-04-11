57d5ba1aea47b57a563f3e5fedbc51e19b80cc0b
ragumagu <shrinidhihr98@gmail.com>
2023-04-11 22:26:22 +0530

    Added grouping pilcrow wip

--- scripts/notation_core.js
+++ scripts/notation_core.js
@@ -10,9 +10,9 @@ aksharas = {
     "ತ": 1,
     "ರಿ": 1,
     "ತ್": 0,
-    "ತಾ": 1,
-    "ತಾಂ": 1,
-    "ತ್ಲಾಂ":1,
+    "ತಾ": 2,
+    "ತಾಂ": 2,
+    "ತ್ಲಾಂ": 2,
     "ತೊ": 1,
     "ತೊಂ": 1,
     "ದಿ": 1,
@@ -25,7 +25,9 @@ aksharas = {
     "ಮಿ": 1,
     "ಣ": 1,
     "ಣು": 1,
-    "ಳಾಂ": 1,
+    "ಳಾಂ": 2,
+    ",": 1,
+    ";": 2
 }
 
 
@@ -35,6 +37,8 @@ function isASCII(str) {
     }
     return /^[\x00-\x7F]*$/.test(str);
 }
+
+var special_characters = ["⁋", "¶"]
 
 function isDiacritic(str) {
     return /^[\u0C80-\u0CFF]*$/.test(str) && !(/^[\u0C85-\u0CB9]*$/.test(str));
@@ -47,7 +51,7 @@ function get_chars(src) {
 
     let i = 0;
     while (i < src.length) {
-        console.log('i is', i);
+        // console.log('i is', i);
         if (src.substring(i, i + 5) in aksharas) {
             result.push(src.substring(i, i + 5));
             i = i + 5;
@@ -60,22 +64,22 @@ function get_chars(src) {
         } else if (src.substring(i, i + 1) in aksharas) {
             result.push(src.substring(i, i + 1));
             i = i + 1;
-        } else if (!isASCII(src[i])) {
+        } else if (!isASCII(src[i]) && !(special_characters.includes(src[i]))) {
             alert("Unrecognized character" + src[i] + ",at index:" + i)
             console.log("Unrecognized character:'" + src[i] + "',at index:" + i);
         } else {
-            console.log("ascii:'" + src[i] + "', at i", i, src.substring(i));
+            // console.log("ascii:'" + src[i] + "', at i", i, src.substring(i));
             if (src.substring(i, i + 2) === "((" || src.substring(i, i + 2) === "))" ||
                 src.substring(i, i + 2) === "||") {
                 result.push(src.substring(i, i + 2))
-                console.log("pushing" + src.substring(i, i + 2))
+                    // console.log("pushing" + src.substring(i, i + 2))
                 i = i + 1;
             } else if (src[i] === " ") { // space
                 if (result[result.length - 1] != " ") {
                     result.push(src[i]);
                 }
             } else if (!(src[i] === " " || src[i] === "\n")) { // &emsp; and new line characters
-                console.log("Pushing ascii:'" + src[i] + "'");
+                // console.log("Pushing ascii:'" + src[i] + "'");
                 result.push(src[i]);
             }
             i = i + 1;
@@ -85,10 +89,10 @@ function get_chars(src) {
     return result;
 }
 
-function generate_table_of_contents(){
+function generate_table_of_contents() {
     let toc = document.createElement("table");
-    book.forEach((page, idx)=>{
-        let link_text = "Page " + (idx+1) +": " + page.substring(0, page.indexOf("\n"));
+    book.forEach((page, idx) => {
+        let link_text = "Page " + (idx + 1) + ": " + page.substring(0, page.indexOf("\n"));
         let link = document.createElement("a");
         link.innerText = link_text;
 
@@ -128,66 +132,54 @@ function get_row(content) {
         if (split[i] in aksharas) {
             text += split[i];
             text_len += aksharas[split[i]];
-        } else if (split[i] === "(") {
-            console.log("FOUnd (");
-            td.appendChild(document.createTextNode(text));
+
+        } else if (split[i] === "(" || split[i] === "((") {
+            if (text != "") {
+                let span = document.createElement("span");
+                span.innerHTML = text;
+                td.appendChild(span);
+            }
+
             text = "";
             text_len = 0;
 
         } else if (split[i] === ")") {
-            let span = document.createElement("span");
-            span.innerHTML = text;
-            span.classList.add("su");
-            td.appendChild(span);
-
-            text = "";
-            text_len = 0;
-        } else if (split[i] === "((") {
-            td.appendChild(document.createTextNode(text));
-            text = "";
-            text_len = 0;
-        } else if (split[i] === "))") {
-            let span = document.createElement("span");
-            span.innerHTML = text;
-            span.classList.add("du");
-            td.appendChild(span);
-
-            text = "";
-            text_len = 0;
-        } else if (split[i] === "||") {
-            if (!(text === "")) {
-                td.appendChild(document.createTextNode(text));
-                row.appendChild(td);
-                text = "";
-                text_len = 0;
+            if (text != "") {
+                let span = document.createElement("span");
+                span.innerHTML = text;
+                span.classList.add("su");
+                td.appendChild(span);
             }
 
-            td = document.createElement("td");
-            td.innerHTML = "||";
-            row.appendChild(td)
+            text = "";
+            text_len = 0;
 
-            td = document.createElement("td");
+        } else if (split[i] === "))") {
+            if (text != "") {
+                let span = document.createElement("span");
+                span.innerHTML = text;
+                span.classList.add("du");
+                td.appendChild(span);
+            }
+            text = "";
+            text_len = 0;
 
-        } else if (split[i] === "|") {
-            td.appendChild(document.createTextNode(text));
-            row.appendChild(td);
+        } else if (["||", "⁋", "¶", "|", "\\"].includes(split[i])) {
+            if (!(text === "")) {
+                let span = document.createElement("span");
+                span.innerHTML = text;
+                td.appendChild(span);
+                row.append(td);
+            }
 
             text = "";
             text_len = 0;
 
-            td = document.createElement("td");
-            td.innerHTML = "|";
-            row.appendChild(td);
-
-            td = document.createElement("td");
-
-        } else if (split[i] === "\\") {
-            console.log("making new column");
-            td.appendChild(document.createTextNode(text));
-            row.appendChild(td);
-
-            text = "";
-            text_len = 0;
+            if (split[i] !== "\\") {
+                td = document.createElement("td");
+                td.innerHTML = split[i];
+                row.appendChild(td)
+            }
 
             td = document.createElement("td");
 
@@ -201,90 +193,110 @@ function get_row(content) {
         console.log('text is:"' + text + '"');
     }
 
-    // row.innerHTML = content;
     return row;
 }
 
 function render_text(text, output_container) {
     let lines = text.split("\n");
+    let buff = "";
 
     let curr_table = null;
     let curr_table_style = null;
     let result = [];
+    let new_table = false;
 
-    lines.forEach((line) => {
-        console.log('line:', line, typeof line);
-        line = line.trim();
-        if (line === "##TOC##"){
-            result.push(generate_table_of_contents());
-        } else if (line.startsWith("[||") && line.endsWith("||]")) {
-            let align = get_row(line.substring(1, line.length - 1));
-            console.log('align', align, line.substring(3, line.length - 3));
-            curr_table_style = {
-                "align-right": [],
-                "align-center": [],
-            }
-            let i = 0;
-            align.childNodes.forEach((td) => {
-                td = td.innerHTML.trim();
-                if (td === "r") {
-                    curr_table_style['align-right'].push(i);
-                } else if (td === "c") {
-                    curr_table_style['align-center'].push(i);
+    for (let i = 0; i < lines.length; i++) {
+        buff = lines[i].trim();
+
+        while (buff !== "") {
+            if (buff === "#TOC") {
+                result.push(generate_table_of_contents());
+                buff = "";
+            } else if (buff[0] === "*") {
+                if (curr_table === null) {
+                    new_table = true;
+                } else {
+                    // Add styles to current table and push to result
+
+                    if (curr_table_style !== null) {
+                        curr_table.childNodes.forEach((tr) => {
+                            curr_table_style['align-right'].forEach((col_num) => {
+                                [...tr.childNodes][col_num].classList.add("text-align-right");
+                            });
+
+                            curr_table_style['align-center'].forEach((col_num) => {
+                                [...tr.childNodes][col_num].classList.add("text-align-center");
+                            });
+                        })
+                    }
+                    result.push(curr_table);
+                    curr_table = null;
+                    curr_table_style = null;
                 }
-                i++;
-            });
-            console.log("current style is", curr_table_style);
-        } else if (line.startsWith("*||") && line.endsWith("||*")) {
-            curr_table = document.createElement("table");
-            curr_table.appendChild(get_row(line.substring(1, line.length - 1)));
-            result.push(curr_table);
-            curr_table = null;
-        } else if (line.startsWith("*||")) {
-            // make new table;
-            curr_table = document.createElement("table");
-            let content = line.substring(1);
-            console.log("content:", content);
-            curr_table.appendChild(get_row(content));
+                buff = buff.substring(1);
+            } else if (buff.startsWith("||") | buff.startsWith("//")) {
+                match_string = buff.substring(0, 2);
+                let markers = ["||", "||"];
 
-        } else if (line.endsWith("||*") && curr_table !== null) {
-            // end table;
-            curr_table.appendChild(get_row(line.substring(0, line.length - 1)));
+                if (match_string === "//") {
+                    markers = ["⁋", "¶"]
+                }
 
-            if (curr_table_style !== null) {
-                curr_table.childNodes.forEach((tr) => {
-                    curr_table_style['align-right'].forEach((col_num)=>{
-                        [...tr.childNodes][col_num].classList.add("text-align-right");
-                    });
+                if (new_table) {
+                    curr_table = document.createElement("table");
+                    new_table = false;
+                }
 
-                    curr_table_style['align-center'].forEach((col_num)=>{
-                        [...tr.childNodes][col_num].classList.add("text-align-center");
-                    });
-                })
+                end = buff.substring(2).indexOf(match_string);
+                if (end < 0) {
+                    throw new Error('Unmatched ' + match_string +
+                        " on line number " + i + ", line:" + lines[i]);
+                }
+
+                curr_table.appendChild(get_row(markers[0] +
+                    buff.substring(2, end + 2) + markers[1]));
+
+                buff = buff.substring(end + 4);
+
+            } else if (buff.startsWith["["] && buff.endsWith("]")) {
+
+                let align = get_row(buff.substring(1, buff.length - 1));
+                curr_table_style = {
+                    "align-right": [],
+                    "align-center": [],
+                }
+                let i = 0;
+                align.childNodes.forEach((td) => {
+                    td = td.innerHTML.trim();
+                    if (td === "r") {
+                        curr_table_style['align-right'].push(i);
+                    } else if (td === "c") {
+                        curr_table_style['align-center'].push(i);
+                    }
+                    i++;
+                });
+
+                buff = "";
+            } else if (buff.startsWith("# ")) {
+                // add h1
+                let h1 = document.createElement("h1");
+                h1.innerHTML = buff.substring(2);
+                result.push(h1);
+                buff = "";
+            } else if (buff.startsWith("## ")) {
+                // add h2
+                let h2 = document.createElement("h2");
+                h2.innerHTML = buff.substring(3);
+                result.push(h2);
+                buff = "";
+            } else {
+                let p = document.createElement("p");
+                p.innerHTML = buff;
+                result.push(p);
+                buff = "";
             }
-            result.push(curr_table);
-            curr_table = null;
-            curr_table_style = null;
-
-        } else if (line.startsWith("||") && curr_table !== null) {
-            // add to current table.
-            curr_table.appendChild(get_row(line));
-
-        } else if (line.startsWith("# ")) {
-            // add h1
-            let h1 = document.createElement("h1");
-            h1.innerHTML = line.substring(2);
-            result.push(h1);
-        } else if (line.startsWith("## ")) {
-            // add h2
-            let h2 = document.createElement("h2");
-            h2.innerHTML = line.substring(3);
-            result.push(h2);
-        } else {
-            let p = document.createElement("p");
-            p.innerHTML = line;
-            result.push(p);
         }
-    })
+    };
     output_container.replaceChildren(...result);
+
 }
